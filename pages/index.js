782d2b8d6e2f2bf86ed48085ba304648b3b4b249
kuldeep4654213 <kuldeep4654213@gmail.com>
2023-04-08 05:49:11 -0400

    link added

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,13 @@
 /*********************************************************************************
-*  WEB422 – Assignment 5
-*  I declare that this assignment is my own work in accordance with Seneca Academic Policy.  
-*  No part of this assignment has been copied manually or electronically from any other source
-*  (including web sites) or distributed to other students.
+*  WEB422 – Assignment 06
+*  I declare that this assignment is my own work in accordance with Seneca  Academic Policy.  No part of this
+*  assignment has been copied manually or electronically from any other source (including web sites) or 
+*  distributed to other students.
 * 
 *  Name: Kuldeep singh
 *  Student ID: 127966216 
 *  Date: 21/03/2023
+*  Vercel App (Deployed) Link: my-app-vert-three.vercel.app
 *
 ********************************************************************************/ 
 
